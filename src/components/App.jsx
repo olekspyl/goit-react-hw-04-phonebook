cc641d392504060f96aef0d034c0d0095e410fa7
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-01-19 19:16:07 +0100

    for checking

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,56 +6,68 @@ import Filter from './Form/Filter';
 import {Title, TitleContacts, TitleFind, WrapToFind} from './Form/App.styled'
 
 
-export default function App({id, name, number}) {
-  const [contacts, setContacts] = useState([]);
+export default function App() {
+  // const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')));
   const [filter, setFilter] = useState('');
-
-
-  useEffect(() => {
-    const contactsStorage = window.localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contactsStorage);
-    setContacts( parsedContacts)
-  }, []);
 
 
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts))
   }, [contacts]);
 
-  const deleteContact = () => {
-      setContacts(prevState => 
-        prevState.contacts.filter(contact => contact.id !== id),
-      );
+  const deleteContact = id => {
+      setContacts(prevState => prevState.filter(contact => contact.id !== id));
     };
 
-  const addContact = () => {
+  
+  
+  
+  const onSubmit = ({ id, name, number }) => {
     if (contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     )) return alert(`Contact ${name} ia already in phonebook`);
     
-    setContacts(prevState => 
-        [...prevState.contacts, { id, name, number }]
+    setContacts( 
+      prevState => [{ id, name, number }, ...prevState]
     );
   };
 
+  // const addContact = data => {
+  //   const array = contacts.filter(
+  //     contact => contact.name.toLowerCase() === data.name.toLowerCase()
+  //   );
+  //   if (array.length > 0) {
+  //     alert(`${data.name} is already in contacts`);
+  //   } else {
+  //     setContacts(prevState => [data, ...prevState]);
+  //   }
+  // };
+
+
+
+  
   const formFilterChange = e => {
     setFilter(e.target.value);
   };
 
-  const normalizedElem = filter.toLowerCase();
-  const filteredElements = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedElem));
-
+  const getVisibleContacts = () => {
+   const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredContacts;
+  }
+  
   return (
     <div>
       <Title>Phonebook</Title>
-      <Form onSubmit={addContact} />
+      <Form onSubmit={onSubmit} />
       <TitleContacts>Contacts</TitleContacts>
       <WrapToFind>
         <TitleFind>Find contacts by name</TitleFind>
         <Filter formFilterChange={formFilterChange} filter={filter} />
       </WrapToFind>
-      <Contacts contacts={filteredElements} onDelete={deleteContact} />
+      <Contacts contacts={getVisibleContacts()} onDelete={deleteContact} />
         
     </div>
   );  
