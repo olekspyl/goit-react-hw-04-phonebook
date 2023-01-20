7acd317e041d837113c8c38e5d7351cf4b36573f
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-01-20 20:52:34 +0100

    mentor helped

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,92 @@
+// import React from 'react';
+// import { useState, useEffect } from 'react';
+// import Form from './Form/Form';
+// import Contacts from "./Form/Contacts";
+// import Filter from './Form/Filter';
+// import {Title, TitleContacts, TitleFind, WrapToFind} from './Form/App.styled'
+
+
+// export default function App() {
+//   // const [contacts, setContacts] = useState([]);
+//   const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts'))??[]);
+//   const [filter, setFilter] = useState('');
+
+
+//   useEffect(() => {
+//     window.localStorage.setItem('contacts', JSON.stringify(contacts))
+//   }, [contacts]);
+
+//   const deleteContact = id => {
+//       setContacts(prevState => prevState.filter(contact => contact.id !== id));
+//     };
+  
+  
+//   const onSubmit = ({ id, name, number }) => {
+//     if (contacts.find(
+//       contact => contact.name.toLowerCase() === name.toLowerCase()
+//     )) return alert(`Contact ${name} ia already in phonebook`);
+    
+//     setContacts( 
+//       prevState => [{ id, name, number }, ...prevState]
+//     );
+//   };
+
+
+
+
+
+  
+//   const formFilterChange = e => {
+//     setFilter(e.target.value);
+//   };
+
+//   const getVisibleContacts = () => {
+//    return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+    
+//   }
+  
+//   return (
+//     <div>
+//       <Title>Phonebook</Title>
+//       <Form onSubmit={onSubmit} />
+//       <TitleContacts>Contacts</TitleContacts>
+//       <WrapToFind>
+//         <TitleFind>Find contacts by name</TitleFind>
+//         <Filter formFilterChange={formFilterChange} filter={filter} />
+//       </WrapToFind>
+//       <Contacts contacts={getVisibleContacts()} onDelete={deleteContact} />
+        
+//     </div>
+//   );  
+// };
+
+
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Form from './Form/Form';
 import Contacts from "./Form/Contacts";
 import Filter from './Form/Filter';
 import {Title, TitleContacts, TitleFind, WrapToFind} from './Form/App.styled'
-
-
 export default function App() {
   // const [contacts, setContacts] = useState([]);
-  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')));
+  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? []);
   const [filter, setFilter] = useState('');
-
-
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts))
   }, [contacts]);
-
   const deleteContact = id => {
       setContacts(prevState => prevState.filter(contact => contact.id !== id));
     };
-
-  
-  
-  
   const onSubmit = ({ id, name, number }) => {
     if (contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     )) return alert(`Contact ${name} ia already in phonebook`);
-    
-    setContacts( 
+    setContacts(
       prevState => [{ id, name, number }, ...prevState]
     );
   };
-
   // const addContact = data => {
   //   const array = contacts.filter(
   //     contact => contact.name.toLowerCase() === data.name.toLowerCase()
@@ -43,21 +97,15 @@ export default function App() {
   //     setContacts(prevState => [data, ...prevState]);
   //   }
   // };
-
-
-
-  
   const formFilterChange = e => {
     setFilter(e.target.value);
   };
-
   const getVisibleContacts = () => {
-   const filteredContacts = contacts.filter(contact =>
+   return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
-    return filteredContacts;
+    // return filteredContacts;
   }
-  
   return (
     <div>
       <Title>Phonebook</Title>
@@ -68,10 +116,6 @@ export default function App() {
         <Filter formFilterChange={formFilterChange} filter={filter} />
       </WrapToFind>
       <Contacts contacts={getVisibleContacts()} onDelete={deleteContact} />
-        
     </div>
-  );  
+  );
 };
-
-
-
