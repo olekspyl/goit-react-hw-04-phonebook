0cbcadb8d8e2d36a1f68b0817e060005e640a8b2
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-02-10 21:29:40 +0100

    delete state in form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,72 +1,41 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { nanoid } from 'nanoid'
 import {FormEl, Label, Button, Input} from './App.styled'
 
 
 export default function Form({onSubmit}) {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
   const id = nanoid();
 
-// ще один варіант рішення
-//   const [form, setForm] = useState({ name: '', number: '' });
-//   const handleChange = e => {    const { name, value } = e.currentTarget;    setForm(prevForm => ({ ...prevForm, [name]: value }));  };
-  
- const handleChange = e => {
-    e.target.value.trim();
-    const { name, value } = e.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-      case 'number':
-        setNumber(value);
-        break;
-      default:
-        return;
-    }
-  }; 
-
-  const reset = () => {
-    setNumber('');
-    setName('');
-  };
-
   const handleSubmit = e => {
-    e.preventDefault();    
+    e.preventDefault();   
     const data = {
-      id,
-      name,
-      number,
+      id: id,
+      name: e.target.name.value.trim(),
+      number: e.target.number.value.trim(),
     };
     onSubmit(data);
-    reset();
+    e.target.reset();
+    
   };
-
 
    return (
     <FormEl onSubmit={handleSubmit}>
-    <Label htmlFor={id}>
+    <Label htmlFor="name">
       Name
       <Input
         id={id}
         type="text"
         name="name"
-        value={name}
-        onChange={handleChange}
         required
       />
     </Label>
-    <Label htmlFor={id}>
+    <Label htmlFor="number">
       Phone number
       <Input
         id={id}
         type="tel"
         name="number"
-        value={number}
-        onChange={handleChange}
         required
       />
     </Label>
